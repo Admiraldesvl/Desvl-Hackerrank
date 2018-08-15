383948c546b5541a367995e866f894a1a398df51
Admiraldesvl <40477748+Admiraldesvl@users.noreply.github.com>
2018-08-15 09:39:50 +0800

    Create ProjectEuler_218FINAL.cpp

---
+++ ProjectEuler_218FINAL.cpp
@@ -0,0 +1,19 @@
+// After struggling for days, I finally made it to a O(1) solution.
+// Don't be surprised. 
+// Just make it clear, A perfect right angled triangle is super perfect.
+// So the answer is always zero.
+// Problem : https://www.hackerrank.com/contests/projecteuler/challenges/euler218
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+  unsigned long q;
+  unsigned long long n;
+  while(q--){
+    cin >> n;
+    cout << 0 << endl;
+  }
+  return 0;
+}
